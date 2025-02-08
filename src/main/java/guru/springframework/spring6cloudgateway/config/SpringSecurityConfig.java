309376b24714b8f2ae9a8bc2a13bcab01e714f9a
John <john.refaat88@gmail.com>
2025-02-08 03:46:43 +0200

    Adding Spring Actuator

--- src/main/java/guru/springframework/spring6cloudgateway/config/SpringSecurityConfig.java
+++ src/main/java/guru/springframework/spring6cloudgateway/config/SpringSecurityConfig.java
@@ -1,7 +1,9 @@
 package guru.springframework.spring6cloudgateway.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -15,9 +17,19 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @EnableWebFluxSecurity
 public class SpringSecurityConfig {
 
+   /* @Bean
+    @Order(1)
+    public SecurityWebFilterChain actuatorSecurityWebFilterChain(ServerHttpSecurity http) {
+        http.securityMatcher(EndpointRequest.toAnyEndpoint())
+                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().permitAll())
+                .build();
+        return http.build();
+    }*/
+
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity security) {
         return security.csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(auth-> auth.pathMatchers("actuator/**").permitAll())
                 .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
                 .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()))
                 .build();
